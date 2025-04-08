20baf416c056e7e9024cb68369bf246cda686eb7
SoulSourav <130975907+SouravH5409@users.noreply.github.com>
2025-04-08 21:20:01 +0530

    Update ftpclient.c

--- FTP/ftpclient.c
+++ FTP/ftpclient.c
@@ -13,8 +13,6 @@ struct sockaddr_in server_addr;
 char buffer[BUFFER_SIZE] = {0};
 char filename[BUFFER_SIZE] = {0};
 
-// Create client socket
-
 client_socket = socket(AF_INET, SOCK_STREAM, 0);
 if (client_socket < 0) {
     perror("Socket creation failed");
@@ -25,20 +23,15 @@ if (client_socket < 0) {
 server_addr.sin_port = htons(PORT);
 server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-// Connect to server
 if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
 perror("Connection failed");
 exit(EXIT_FAILURE);
 
 }
-// Get filename from user
 printf("Enter the filename to request: ");
 scanf("%s", filename);
 
-// Send filename to server
 send(client_socket, filename, strlen(filename), 0);
-
-// Receive and print file content
 
 printf("File content received:\n");
 while (recv(client_socket, buffer, BUFFER_SIZE, 0) > 0) {
